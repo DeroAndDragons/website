e7f1162c61c25474b00f8c117b182e0307117fdc
g45t345rt <fred10.cha@gmail.com>
2022-03-17 12:25:03 -0400

    test no build with h()

--- functions/index.tsx
+++ functions/index.tsx
@@ -1,11 +1,15 @@
 import render from 'preact-render-to-string'
 import { h } from 'preact'
 
-const App = <div class="foo">content</div>
+const App = h("html", null,
+  h("body", null,
+    h("div", null, "ok"),
+  ),
+)
 
 export async function onRequest(context) {
   return new Response(
-    render(App),
+    render(App, null, { pretty: true }),
     { headers: { 'Content-Type': 'text/html' } }
   )
 } 
