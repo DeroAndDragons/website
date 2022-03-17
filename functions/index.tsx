a79107dfd0e0a5276d03e38d1d26c9cacfcd10be
g45t345rt <fred10.cha@gmail.com>
2022-03-17 12:40:13 -0400

    test context

--- functions/index.tsx
+++ functions/index.tsx
@@ -1,15 +1,19 @@
 import render from 'preact-render-to-string'
 import { h } from 'preact'
 
-const App = h("html", null,
-  h("body", null,
-    h("div", null, "ok"),
-  ),
-)
+const App = (props) => {
+  return h("html", null,
+    h("body", null,
+      h("div", null, "ok"),
+      h("div", null, JSON.stringify(props)),
+      h("img", { src: "/static/test.jpg" })
+    )
+  )
+}
 
 export async function onRequest(context) {
   return new Response(
-    render(App, null, { pretty: true }),
+    render(h(App, { context }), null, { pretty: true }),
     { headers: { 'Content-Type': 'text/html' } }
   )
 } 
